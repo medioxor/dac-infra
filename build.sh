9f44eddecd5f2a355b5c0bd79b76c0fad2c58bcd
medioxor <medioxor@gmail.com>
2025-02-10 21:00:29 +1000

    updated windows 11 iso, fixed answers file due to 24h2 changes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cd packer
+packer init config.pkr.hcl
 dotenv -e ../.env packer build -var-file variables.json ubuntu_server.json
 dotenv -e ../.env packer build -var-file variables.json windows_11.json
 cd -
